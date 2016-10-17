0e422699d558044c3d6822502daa0aa0b2d53575
Dan Radez <dradez@redhat.com>
2016-10-17 09:05:29 -0400

    Populating init.pp and parameters
    - TODO: acceptence tests
    
    Change-Id: Ia8124029da59cdf51f60f20073ba6918dc395180
    Signed-off-by: Dan Radez <dradez@redhat.com>

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,15 +1,22 @@
 # Parameters for puppet-tacker
 #
 class tacker::params {
+  include ::openstacklib::defaults
 
+  $client_package_name = 'python-tackerclient'
   case $::osfamily {
     'RedHat': {
+      $package_name                 = 'openstack-tacker'
+      $service_name                 = 'tacker-server'
+      $service_provider             = undef
     }
     'Debian': {
+      $package_name                 = 'tacker'
+      $service_name                 = 'tacker'
+      $service_provider             = undef
     }
     default: {
       fail("Unsupported osfamily: ${::osfamily} operatingsystem")
-    }
-
-  } # Case $::osfamily
+    } # Case $::osfamily
+  }
 }

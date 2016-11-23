a86c2e1907c6743a9f419e144cc0d851d772e88d
Dan Radez <dradez@redhat.com>
2016-11-23 10:00:06 -0500

    Adding a client.pp file
    Adding a class that will handle installing the tacker python client pkg
    
    Change-Id: I8c6690882a61e018bf6c2803d3340bced32e79ee
    Signed-off-by: Dan Radez <dradez@redhat.com>

--- manifests/params.pp
+++ manifests/params.pp
@@ -6,14 +6,12 @@ class tacker::params {
   $client_package_name = 'python-tackerclient'
   case $::osfamily {
     'RedHat': {
-      $package_name                 = 'openstack-tacker'
-      $service_name                 = 'tacker-server'
-      $service_provider             = undef
+      $package_name     = 'openstack-tacker'
+      $service_name     = 'tacker-server'
     }
     'Debian': {
-      $package_name                 = 'tacker'
-      $service_name                 = 'tacker'
-      $service_provider             = undef
+      $package_name     = 'tacker'
+      $service_name     = 'tacker'
     }
     default: {
       fail("Unsupported osfamily: ${::osfamily} operatingsystem")

1288f2d820c0de1186d2e5834eac5acc271a57b7
ZhongShengping <chdzsp@163.com>
2020-03-25 14:53:41 +0800

    Deprecate min_pool_size option
    min_pool_size option is not used，see:
    
    https://review.opendev.org/#/c/565090/
    
    Change-Id: I75139cc57469827bfa6452883f7d1bd13be48e08
    Closes-Bug: #1868511

--- spec/classes/tacker_db_spec.rb
+++ spec/classes/tacker_db_spec.rb
@@ -9,7 +9,6 @@ describe 'tacker::db' do
         :db_max_retries          => '<SERVICE DEFAULT>',
         :connection              => 'sqlite:////var/lib/tacker/tacker.sqlite',
         :connection_recycle_time => '<SERVICE DEFAULT>',
-        :min_pool_size           => '<SERVICE DEFAULT>',
         :max_pool_size           => '<SERVICE DEFAULT>',
         :max_retries             => '<SERVICE DEFAULT>',
         :retry_interval          => '<SERVICE DEFAULT>',
@@ -23,7 +22,6 @@ describe 'tacker::db' do
         {
           :database_connection              => 'mysql+pymysql://tacker:tacker@localhost/tacker',
           :database_connection_recycle_time => '3601',
-          :database_min_pool_size           => '2',
           :database_max_retries             => '11',
           :database_db_max_retries          => '-1',
           :database_retry_interval          => '11',
@@ -38,7 +36,6 @@ describe 'tacker::db' do
       it { should contain_oslo__db('tacker_config').with(
         :connection              => 'mysql+pymysql://tacker:tacker@localhost/tacker',
         :connection_recycle_time => '3601',
-        :min_pool_size           => '2',
         :max_pool_size           => '11',
         :max_retries             => '11',
         :db_max_retries          => '-1',

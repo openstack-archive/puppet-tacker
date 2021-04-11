478293eedccd545bbde25548957288020d0a797c
Thomas Goirand <zigo@debian.org>
2021-04-11 23:46:39 +0200

    Allow to configure policy_dirs
    This patch makes it possible to override the current service default,
    which is /etc/<service>/policy.d .
    
    Change-Id: I006c137e78c1bac99202ced38fd6f82a07ad65e3

--- manifests/policy.pp
+++ manifests/policy.pp
@@ -32,11 +32,16 @@
 #   (Optional) Path to the tacker policy.yaml file
 #   Defaults to /etc/tacker/policy.yaml
 #
+# [*policy_dirs*]
+#   (Optional) Path to the tacker policy folder
+#   Defaults to $::os_service_default
+#
 class tacker::policy (
   $enforce_scope        = $::os_service_default,
   $enforce_new_defaults = $::os_service_default,
   $policies             = {},
   $policy_path          = '/etc/tacker/policy.yaml',
+  $policy_dirs          = $::os_service_default,
 ) {
 
   include tacker::deps
@@ -56,7 +61,8 @@ class tacker::policy (
   oslo::policy { 'tacker_config':
     enforce_scope        => $enforce_scope,
     enforce_new_defaults => $enforce_new_defaults,
-    policy_file          => $policy_path
+    policy_file          => $policy_path,
+    policy_dirs          => $policy_dirs,
   }
 
 }

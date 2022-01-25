37046e86a4f8fbb1d48c69191f1980143d02b164
Takashi Kajinami <tkajinam@redhat.com>
2022-01-25 10:22:41 +0900

    Accept system scope credentials for Keystone API request
    This change is the first step to support secure RBAC and allows usage
    of system scope credentials for Keystone API request.
    
    This change covers the following two items.
     - assignment of system scope roles to system user
     - credential parameters for authtoken middleware
    
    Depends-on: https://review.opendev.org/804325
    Change-Id: I8bd8e455c739d6eaa43d3ea22af86003a7617bc9

--- manifests/keystone/auth.pp
+++ manifests/keystone/auth.pp
@@ -19,6 +19,18 @@
 #   (Optional) Tenant for tacker user.
 #   Defaults to 'services'.
 #
+# [*roles*]
+#   (Optional) List of roles assigned to tacker user.
+#   Defaults to ['admin']
+#
+# [*system_scope*]
+#   (Optional) Scope for system operations.
+#   Defaults to 'all'
+#
+# [*system_roles*]
+#   (Optional) List of system roles assigned to tacker user.
+#   Defaults to []
+#
 # [*configure_endpoint*]
 #   (Optional) Should tacker endpoint be configured?
 #   Defaults to true.
@@ -67,6 +79,9 @@ class tacker::keystone::auth (
   $auth_name           = 'tacker',
   $email               = 'tacker@localhost',
   $tenant              = 'services',
+  $roles               = ['admin'],
+  $system_scope        = 'all',
+  $system_roles        = [],
   $configure_endpoint  = true,
   $configure_user      = true,
   $configure_user_role = true,
@@ -100,6 +115,9 @@ class tacker::keystone::auth (
     password            => $password,
     email               => $email,
     tenant              => $tenant,
+    roles               => $roles,
+    system_scope        => $system_scope,
+    system_roles        => $system_roles,
     public_url          => $public_url,
     internal_url        => $internal_url,
     admin_url           => $admin_url,
